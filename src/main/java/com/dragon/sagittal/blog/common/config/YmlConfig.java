af692ab3172ae018438c0c4d83e47173bb6636df
sagittal98 <863524199@qq.com>
2021-08-30 21:09:46 +0800

    id生成器

---
+++ src/main/java/com/dragon/sagittal/blog/common/config/YmlConfig.java
@@ -0,0 +1,23 @@
+package com.dragon.sagittal.blog.common.config;
+
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Data
+public class YmlConfig {
+
+    /**
+     * 数据中心
+     */
+    @Value("${yml.dataCenterId}")
+    private long dataCenterId;
+    /**
+     * 机器码
+     */
+    @Value("${yml.machineId}")
+    private long machineId;
+
+}
